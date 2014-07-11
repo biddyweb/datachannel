0eb7a2bb852d4202d3adb9558dd40c682962c6c2
Pasquale Boemio <boemianrapsodi@gmail.com>
2014-07-11 14:34:31 +0200

    travis

--- test/rtcpeerconnection.test.cc
+++ test/rtcpeerconnection.test.cc
@@ -15,20 +15,18 @@ TEST(PeerConnection, shouldExist) {
   delete pc;
 }
 
-static void onOfferReady(RTCSessionDescription* sd) {
-  ASSERT_TRUE(sd->getType() == "offer");
-  printf("type: %s\n%s", sd->getType().c_str(), sd->getSdp().c_str());
-  delete sd;
-  delete pc;
-
-  closetest = true;
-  cv.notify_all();
-}
-
 TEST(PeerConnection, shouldCreateAnOffer) {
   pc = new RTCPeerConnection();
 
-  pc->createOffer(onOfferReady);
+  pc->createOffer([](RTCSessionDescription* sd) {
+    ASSERT_TRUE(sd->getType() == "offer");
+    printf("type: %s\n%s", sd->getType().c_str(), sd->getSdp().c_str());
+    delete sd;
+    delete pc;
+
+    closetest = true;
+    cv.notify_all();
+  });
 
   cv.wait(lk, []{ return closetest; });
   closetest = false;
